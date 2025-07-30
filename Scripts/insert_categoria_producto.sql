f2e17efa415b2923e30f76e4a61fe27fc1cc9efb
Ricardo <ricardopsj3043@gmail.com>
2025-07-31 00:11:25 +0200

    update 1.0.2

--- Scripts/insert_categoria_producto.sql
+++
@@ -1,4 +0,0 @@
-USE shadowstore;
-
-INSERT INTO categoria_producto (id_categoria, nombre_categoria, descripcion_categoria) VALUES
-(1, 'Juegos', 'Recargas y monedas para videojuegos');
--- Scripts/insert_producto.sql
+++
@@ -1,6 +0,0 @@
-USE shadowstore;
-
-INSERT INTO producto (id_producto, nombre_producto, descripcion_producto, imagen_producto, estado_producto, id_categoria) VALUES
-(1, 'Free Fire', 'Recargas de diamantes para Free Fire', 'freefire.jpg', 'activo', 1),
-(2, 'Mobile Legends', 'Recargas de diamantes para Mobile Legends', 'mobilelegends.jpg', 'activo', 1),
-(3, 'Call of Duty', 'Recargas para Call of Duty', 'cod.jpg', 'activo', 1);
